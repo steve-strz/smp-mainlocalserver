acdde164c282dd677c53cd7ff818b8ad03224561
stevestrz <pro.stevestrz@gmail.com>
2021-07-10 19:58:18 +0200

    bluetooth again

--- scripts/bluetooth/searchDevices.sh
+++ scripts/bluetooth/searchDevices.sh
@@ -6,9 +6,9 @@ set address [lindex $argv 0]
 spawn sudo bluetoothctl
 expect -re $prompt
 send "scan on\r"
-send_user "\n[i] Searching bluetooth devices\r"
+send_user "\nSearching bluetooth devices\r"
 sleep 10
-send_user "\n[i] Done searching bluetooth devices\r"
+send_user "\nDone searching bluetooth devices\r"
 send "scan off\r"
 send "quit\r"
 expect eof
